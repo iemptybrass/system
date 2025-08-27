06ac12dac7a50ca9a32277e1dddefacfe681b5f8
iemptybrass <iemptybrass@users.noreply.github.com>
2025-08-27 17:24:26 -0400

    Edit: File: /nixos/flake.nix
    Added Boilerplate

---
+++ nixos/flake.nix
@@ -0,0 +1,52 @@
+{
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+    hardware = {
+      url = "github:nix-community/nixos-facter-modules";
+    };
+  };
+
+  outputs = {
+      self,
+      nixpkgs,
+      hardware,
+      ...
+    } @ inputs:
+    let
+      hostname = "nixos";
+      version = "XX.XX";
+      identifier = "XXXXXXXX";
+    in {
+
+      nixosConfigurations = {
+        "${hostname}" = nixpkgs.lib.nixosSystem {
+
+          specialArgs = {
+            inherit
+              inputs
+              hostname
+              version
+              identifier;
+          };
+
+          modules = [
+            {
+            
+              imports = [ 
+                hardware.nixosModules.facter
+              ];
+              
+              config.facter.reportPath = ./facter.json; 
+              
+            }
+          ];
+
+        };
+      };
+
+    };
+
+}
